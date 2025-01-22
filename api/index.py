4b33fa8425fa34d0d60e3907275240a4e02fe527
Leko <ljankovi@caltech.edu>
2025-01-22 12:49:09 -0800

    mess2

--- api/index.py
+++ api/index.py
@@ -1,14 +1,17 @@
- 
-from http.server import BaseHTTPRequestHandler
-from os.path import join
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        with open(join('data', 'file.txt'), 'r') as file:
-          for line in file:
-            self.wfile.write(line.encode())
-        return
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # This allows all domains, you can also configure specific domains
+
+@app.route('/api/functions', methods=['GET'])
+def get_functions():
+    functions = [
+        "function example1() { console.log('Example 1'); }",
+        "function example2() { console.log('Example 2'); }",
+        "function example3() { console.log('Example 3'); }"
+    ]
+    return jsonify(functions)
+
+if __name__ == "__main__":
+    app.run()
